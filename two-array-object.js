b812cd9b9079dda003a22c73e04efdce87ca4b23
Andrew Jensen <andrewkjensen@gmail.com>
2022-07-15 16:05:02 -0700

    complet two-array-object.js

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,26 @@
+"use strict";
+
+/**
+ * create an object with the keys coming from one array and the values coming from a second
+ * if more keys than values, fill extra keys with null
+ * if more values than keys, ignore them
+ */
+
+//create empty Object
+//for loop through array of keys (use i index) and push keyvalues into object
+//check array lengths
+//use conditional to add nulls to extra keys if key array is longer
+//
+
 // add whatever parameters you deem necessary & write docstring
-function twoArrayObject() {
+function twoArrayObject(keys, values) {
+
+  let obj = {};
+
+  for (let i=0; i<keys.length; i++) {
+    values[i]?
+    obj[keys[i]] = values[i]:
+    obj[keys[i]] = null;
+  }
+  return obj;
 }
