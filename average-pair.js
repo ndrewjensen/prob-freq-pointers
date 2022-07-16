e800f613bb5f695b75c92837ce3c1c5e26a4bb7d
Andrew Jensen <andrewkjensen@gmail.com>
2022-07-15 17:05:42 -0700

    refactor average pair to two-point and begin separate positive

--- average-pair.js
+++ average-pair.js
@@ -11,7 +11,6 @@ receives array and a target number, checks if array contains a pair that average
 
 
 function averagePair(nums,target) {
-  if(nums.length === 0) return false;
 
   let left = 0;
   let right = nums.length - 1;
--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,33 @@
-// add whatever parameters you deem necessary & write docstring
-function separatePositive() {
-}
+"use strict";
+
+//two pointers
+//find negative left value, find positive right value, switch them
+//move in with both pointers
+//return array
+
+/** pass in an array of integers
+ * mutate array:
+ * move positive integers to left of array and negs to right
+ * return array (not necessarily sorted)
+ */
+
+
+//FIXME: the while loop is infinite and prevents the test file from checking the other js problems
+// function separatePositive(integers) {
+//   let left = 0;
+//   let right = integers.length - 1;
+
+//   while (left < right) {
+//     if (integers[left] < 0 && integers[right > 0]) {
+//       [integers[left],integers[right]] = [integers[right],integers[left]];
+//     } else if (integers[left] > 0 && integers[right > 0]) {
+//       left ++;
+//     } else if (integers[left] < 0 && integers[right < 0]) {
+//       right --;
+//     } else if (integers[left] > 0 && integers[right < 0]) {
+//       left ++;
+//       right --;
+//     }
+//   }
+//   return integers;
+// }
